d2d084c8ac3d784f8e75274dea111e4bf0956ab8
Guangyuan Yang <yzgyyang@outlook.com>
2018-07-04 00:46:56 -0400

    Update jails/gitlab_runner_ezjail.sh: Add NAT configuration so the net works

--- jails/gitlab_runner_ezjail.sh
+++ jails/gitlab_runner_ezjail.sh
@@ -1,6 +1,19 @@
 #!/bin/sh -x
 
 # NAT
+sysrc pf_enable="YES"
+sysrc gateway_enable="YES"
+mv /etc/pf.conf /etc/pf.conf.orig || true
+# Assume IP_PUB, vtnet0
+echo 'IP_PUB="45.63.11.65"' > /etc/pf.conf
+echo 'IP_JAIL="192.168.0.2"' >> /etc/pf.conf
+echo 'NET_JAIL="192.168.0.0/24"' >> /etc/pf.conf
+echo 'PORT_JAIL="{80,443,2020}"' >> /etc/pf.conf
+echo 'scrub in all' >> /etc/pf.conf
+echo 'nat pass on vtnet0 from $NET_JAIL to any -> $IP_PUB' >> /etc/pf.conf
+echo 'rdr pass on vtnet0 proto tcp from any to $IP_PUB port $PORT_JAIL -> $IP_JAIL' >> /etc/pf.conf
+pfctl -nf /etc/pf.conf
+service pf start
 
 # pkg
 ASSUME_ALWAYS_YES=yes pkg bootstrap
@@ -17,14 +30,14 @@ sysrc ipv4_addrs_lo1="192.168.0.1-9/29"
 service netif cloneup
 
 # Advanced network setting for jails
-echo "security.jail.allow_raw_sockets=1" >> /etc/sysctl.conf
+echo 'security.jail.allow_raw_sockets=1' >> /etc/sysctl.conf
 
 # Install gitlab_runner
-pkg install --yes gitlab-runner
-sysrc gitlab_runner_enable="YES"
+pkg install --yes gitlab-runner tmux
+# It appears that gitlab-runner doesn't work well with FreeBSD's rc system
+#sysrc gitlab_runner_enable="YES"
 
-# Register gitlab_runner
-gitlab-runner register
+echo 'Register using `gitlab-runner register`'
 
-# You need to manually start builds processing: 
-# $ gitlab-runner run    
+echo 'You need to manually start builds processing:'
+echo 'gitlab-runner run'
